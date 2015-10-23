c85e2a1075d2989baae0c7f312444613db0314a7
Anurag Vasanwala <anuragvasanwala@gmail.com>
2015-10-23 22:46:15 +0530

    Initial

---
+++ Windows 10 IoT Core - UltraSonic Distance Mapper (UWP)/Library/DistanceMapper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Windows_10_IoT_Core___UltraSonic_Distance_Mapper__UWP_.Library
+{
+    public static class DistanceMapper
+    {
+        private static Grid _Grid;
+        private static Ellipse _Ellipse;
+        private static Line _Line;
+        private static CompositeTransform _CompositeTransform;
+        private static RowDefinition _RowDefination;
+
+        public static Grid GetMapper(double Angle, int Distance)
+        {
+            _CompositeTransform = new CompositeTransform();
+            _CompositeTransform.Rotation = Angle;
+            
+            _Grid = new Grid();
+            _Grid.RenderTransform = _CompositeTransform;
+
+            /* <RowDefination Height="Auto"/> */
+            _RowDefination = new RowDefinition();
+            _RowDefination.Height = GridLength.Auto;
+            _Grid.RowDefinitions.Add(_RowDefination);
+
+            /* <RowDefination Height="*"/> */
+            _RowDefination = new RowDefinition();
+            _Grid.RowDefinitions.Add(_RowDefination);
+
+            _Grid.HorizontalAlignment = HorizontalAlignment.Center;
+            _Grid.VerticalAlignment = VerticalAlignment.Bottom;
+            
+            _Grid.RenderTransformOrigin = new Windows.Foundation.Point(0, 1);
+
+            _Ellipse = new Ellipse();
+            _Ellipse.Height = 2;
+            _Ellipse.Width = 2;
+            _Ellipse.RenderTransformOrigin = new Windows.Foundation.Point(0, 0);
+            _Ellipse.Margin = new Thickness(-2, 0, -2, 0);
+
+            if(Distance < 50)
+            {
+                _Ellipse.Fill = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+            }
+            else if(Distance < 100)
+            {
+                _Ellipse.Fill = new SolidColorBrush(Color.FromArgb(255, 200, 100, 0));
+            }
+            else
+            {
+                _Ellipse.Fill = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0));
+            }
+
+            _Grid.Children.Add(_Ellipse);
+
+            _Line = new Line();
+            Grid.SetRow(_Line, 1);
+            _Line.X1 = 0;
+            _Line.X2 = 0;
+            _Line.Y1 = Distance + 3;
+            _Line.Y2 = 0;
+            _Line.StrokeThickness = 1;
+            _Line.Stroke = _Ellipse.Fill;
+
+            _Grid.Children.Add(_Line);
+
+            return _Grid;
+        }
+
+    }
+}
---
+++ Windows 10 IoT Core - UltraSonic Distance Mapper (UWP)/MainPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows_10_IoT_Core___UltraSonic_Distance_Mapper__UWP_.Library;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Windows_10_IoT_Core___UltraSonic_Distance_Mapper__UWP_
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Task t1 = new Task(x);
+            t1.Start();
+        }
+
+
+        bool dir = false;
+        int Objects_C = 0;
+        bool Halt = true;
+        public async void x()
+        {
+            Random Rnd = new Random(0);
+
+            while (true)
+            {
+                while (Halt)
+                {
+
+                }
+                await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                                        () =>
+                                        {
+                                            if (a1.Angle < 70 && dir == false)
+                                            {
+                                                a1.Angle += 0.5;
+                                            }
+                                            else if (a1.Angle >= 70 && dir == false)
+                                            {
+                                                dir = true;
+                                            }
+
+                                            if (a1.Angle > -70 && dir == true)
+                                            {
+                                                a1.Angle -= 0.5;
+                                            }
+                                            else if (a1.Angle <= -70 && dir == true)
+                                            {
+                                                dir = false;
+                                            }
+
+
+
+                                           // if (Rnd.Next(0, 50) == 49)
+                                            {
+                                                //if (Grid_Mapper.Children.Count > 140)
+                                                {
+                                                  //  Grid_Mapper.Children.RemoveAt(0);
+                                                }
+                                                if(a1.Angle==-70 || a1.Angle==70)
+                                                {
+                                                    Grid_Mapper.Children.Clear();
+                                                }
+
+                                                if (a1.Angle > -70 && a1.Angle < -60)
+                                                {
+                                                    Grid_Mapper.Children.Add(DistanceMapper.GetMapper(a1.Angle, Rnd.Next(190, 200)));
+                                                }
+
+                                                if (a1.Angle > -60 && a1.Angle < -0)
+                                                {
+                                                    Grid_Mapper.Children.Add(DistanceMapper.GetMapper(a1.Angle, Rnd.Next(100, 120)));
+                                                }
+
+                                                if (a1.Angle > 0 && a1.Angle < 20)
+                                                {
+                                                    Grid_Mapper.Children.Add(DistanceMapper.GetMapper(a1.Angle, Rnd.Next(250, 260)));
+                                                }
+
+                                                if (a1.Angle > 20 && a1.Angle < 50)
+                                                {
+                                                    Grid_Mapper.Children.Add(DistanceMapper.GetMapper(a1.Angle, Rnd.Next(250, 290)));
+                                                }
+
+                                                if (a1.Angle > 50 && a1.Angle < 60)
+                                                {
+                                                    Grid_Mapper.Children.Add(DistanceMapper.GetMapper(a1.Angle, Rnd.Next(49, 70)));
+                                                }
+
+                                                if (a1.Angle > 61 && a1.Angle < 70)
+                                                {
+                                                    Grid_Mapper.Children.Add(DistanceMapper.GetMapper(a1.Angle, Rnd.Next(70, 150)));
+                                                }
+
+
+                                            }
+
+
+                                        });
+                await Task.Delay(1);
+            }
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Halt = false;
+        }
+    }
+}
